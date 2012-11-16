45809dae6220847ab87230309693209f44409a6a
John Evans <prujohn@gmail.com>
2012-11-16 09:03:11 -0600

    removing html platform files from core lib.

--- lib/extensions/controls/html/tab_control/src/tab_selected_event_args.dart
+++
@@ -1,9 +0,0 @@
-part of tabcontrol_control_extensions_buckshot;
-
-
-class TabSelectedEventArgs extends EventArgs
-{
-  final TabItem tab;
-
-  TabSelectedEventArgs(this.tab);
-}
--- lib/extensions/platforms/svg/svg_surface_element.dart
+++
@@ -1,6 +0,0 @@
-part of svg_surface_buckshot;
-
-abstract class SvgSurfaceElement extends SurfaceElement
-{
-  SVGElement rawElement;
-}

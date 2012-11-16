b85ec4cc35dfe0f4e83e3c9695ef0ad64d9abaf4
John Evans <prujohn@gmail.com>
2012-11-16 09:34:41 -0600

    removed html platform demos.

--- example/starter_projects/starter_project/web/models/model.dart
+++
@@ -1,9 +0,0 @@
-part of start_project_buckshot;
-
-class Model {
-
-  // Typically your model will contain methods to exchange data with some
-  // source.
-
-  String title = 'Buckshot Starter Template';
-}
